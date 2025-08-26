5644c9d12c078a2fbbdd218748c891831a60fd60
Lushanthi-Dhananjali <dhananjalijaya@gmail.com>
2025-08-26 20:27:33 +0530

    new

--- backend/src/server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-const { connect } = require('./db');
-
-const PORT = process.env.PORT || 4000;
-
-connect(process.env.MONGO_URI).then(() => {
-  app.listen(PORT, () => console.log(`API on http://localhost:${PORT}`));
-});
